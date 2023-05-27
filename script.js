11529fad274ff62946aa94b13dbcc602a1aedf66
0thigs <machinimavlogs@gmail.com>
2023-05-27 01:25:58 -0300

    Feat: Add Home

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Javascript Conectado!")
+/*
+Bem-vindo(a) à StarDust!
+
+Somos um grupo apaixonado chamado StarDust, dedicado a ajudar pessoas interessadas em ingressar no incrível mundo da programação. Nosso propósito é proporcionar a oportunidade de aprender toda a lógica por trás da programação de uma forma envolvente e divertida, por meio de nosso aplicativo de aprendizado gamificado.
+
+Acreditamos que a programação é uma habilidade essencial nos dias de hoje e que todos têm o potencial de se tornar excelentes programadores. No entanto, sabemos que o processo de aprendizado pode ser desafiador e intimidante para muitos iniciantes. É por isso que criamos a StarDust, com a missão de simplificar o aprendizado da programação e torná-lo acessível a todos.
+
+Nosso aplicativo combina elementos de jogos com lições interativas, oferecendo uma abordagem única para o ensino da programação. Queremos que você se divirta enquanto aprende os conceitos fundamentais da lógica de programação, resolvendo desafios emocionantes e participando de aventuras virtuais.
+
+Nossa equipe é composta por profissionais experientes e entusiastas da programação, apaixonados por compartilhar seu conhecimento e ajudar os outros a desenvolverem suas habilidades. Estamos constantemente atualizando e aprimorando nosso aplicativo para fornecer a melhor experiência de aprendizado possível.
+
+Na StarDust, acreditamos que todos podem se tornar mestres na arte da programação, independentemente de sua formação ou experiência anterior. Se você é um iniciante completo ou já possui algum conhecimento prévio, nosso aplicativo oferece um caminho estruturado para desenvolver suas habilidades e alcançar seus objetivos na programação.
+
+Junte-se a nós na jornada para desvendar os segredos da programação. Estamos animados para ajudá-lo a construir uma base sólida de conhecimento, capacitando você a criar soluções inovadoras e impactar o mundo com suas habilidades de programação.
+
+Seja bem-vindo(a) à StarDust - onde a aprendizagem se transforma em uma emocionante aventura!
+
+Equipe StarDust
+*/
